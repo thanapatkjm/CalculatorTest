4315843311028a8f2ab1c65352ab2ecefc490e68
Dxcuer <thanapatkjm@hotmail.com>
2019-02-07 14:03:03 +0700

    Create Templates and config settings

--- exams/calculator/views.py
+++ exams/calculator/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
 
 # Create your views here.
 def multi_table(request, number):
-    
+    num = number
+    return render(request, 'table.html', {'number':number})
